312209262128fb8a8f741268fe3719e7aecb9164
gittocentro <76614654+gittocentro@users.noreply.github.com>
2023-04-17 10:32:14 +0200

    Update index.js

--- index.js
+++ index.js
@@ -175,9 +175,7 @@ app.use(express.urlencoded({extended: true}))
 
 app.use(session(sessionOptions))
 app.use(mongoSanitize())
-app.use("", (req,res,next) => {
-    helmet({ contentSecurityPolicy: false, })
-})
+
 
 app.get("/", catchAsync(async(req,res) => {
     const user = await User.findById(req.session.user_id)
